9027405cedce6fe112e0ec52d3161d8f1e177556
Julia Grabovskaya <julsuniverse@gmail.com>
2017-12-20 15:21:08 +0000

    auth

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,11 @@ Route::get('articles/cat/{cat_alias?}', [
 Route::resource('comment', 'CommentController', [
     'only' => ['store']
 ]);
+
+//Route::auth();
+
+Route::get('login', 'Auth\LoginController@showLoginForm');
+
+Route::post('login', 'Auth\LoginController@login');
+
+Route::get('logout', 'Auth\LoginController@logout');
