e1943333e9a0338bcaf400111e0a6c6f168bd93d
Julia <julsuniverse@gmail.com>
2017-12-21 15:19:35 +0200

    users roles added

--- app/User.php
+++ app/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Article');
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany('App\Role', 'role_user');
+    }
 }
