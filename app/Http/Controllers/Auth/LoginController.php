df603fcadd765d77feaff94497c7d09c8d67230b
Julia <julsuniverse@gmail.com>
2018-01-29 18:16:24 +0200

    commit changes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -35,15 +35,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-    public function showLoginForm()
-    {
-        return view(env('THEME').'.login');
-    }
-
-    public function username()
-    {
-        return 'login';
     }
 }
